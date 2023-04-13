d7e6928a984fabe29c0a40cf5e9f6ec3c5c7aaa3
Jysus Martinez <jesusavg2317@gmail.com>
2023-04-12 22:18:23 -0600

    Clase 1

--- service/client-service.js
+++ service/client-service.js
@@ -1,6 +1,6 @@
-//backticks
-const crearNuevaLinea = (nombre, email) => {
-  const linea = document.createElement("tr");
+//backticks ``
+const crearNuevoCliente = (nombre,email)=>{
+  const linea = document.createElement("tr")
   const contenido = `
     <td class="td" data-td>
       ${nombre}
@@ -27,44 +27,56 @@ const crearNuevaLinea = (nombre, email) => {
   linea.innerHTML = contenido;
   return linea;
 };
+const table = document.querySelector("[data-table]")
 
-const table = document.querySelector("[data-table]");
 
-//Abrir http (método,url)
+//Abrir http (metodo, url)
+//CRUD
+//CREATE  ->  METODOS HTTP
+//Create  ->  POST
+//Read    ->  GET
+//Update  ->  PUT/PATCH
+//Delete  ->  DELETE
+const thenAgrega = (info) =>{
+  const informacion = info;
+  informacion.forEach((usuarios)=>{
+    const nuevoCliente = crearNuevoCliente (usuarios.nombre,usuarios.email);
+    table.appendChild(nuevoCliente);
+  })
+};
 
-// CRUD   - Métodos HTTP
-// Create - POST
-// Read   - GET
-// Update - PUT/PATCH
-// Delete - DELETE
-
-const listaClientes = () => {
-  const promise = new Promise((resolve, reject) => {
+const listaClientes = ()=>{
+  const promise = new Promise((resolve,reject)=>{
     const http = new XMLHttpRequest();
-    http.open("GET", "http://localhost:3000/perfiles");
-
+    http.open("GET","http://localhost:3000/perfil");
     http.send();
-
-    http.onload = () => {
-      const response = JSON.parse(http.response);
-      if (http.status >= 400) {
-        reject(response);
-      } else {
-        resolve(response);
-      }
+    http.onload = ()=>{
+    const response = JSON.parse(http.response);
+    if(http.status >= 400){
+      reject(response)
+    }else{
+      resolve(response)
+    }
     };
   });
   return promise;
 };
 
 listaClientes()
-  .then((data) => {
-    data.forEach((perfil) => {
-      const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-      table.appendChild(nuevaLinea);
-    });
-  })
-  .catch((error) => alert("Ocurrió un error"));
+  .then((info)=>thenAgrega(info))
+  .catch((error)=>alert("Ocurrio un error"));
 
-// console.log(data);
-//
+
+
+
+  //then
+  //(info)=>{
+  //  info.forEach((usuarios) =>{
+  //    const nuevoCliente = crearNuevoCliente(usuarios.nombre,usuarios.email);
+  //    table.appendChild(nuevoCliente);
+  //  });
+  //  }
+
+
+
+
